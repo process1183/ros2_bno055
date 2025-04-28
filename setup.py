bdb87f852a76bdad50552ecd960eacebdac8d6cb
Josh Gadeken <josh@steelcaverobotics.com>
2025-04-27 20:19:37 -0600

    Update verson to 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'ros2_bno055'
 
 setup(
     name=package_name,
-    version='0.2.1',
+    version='0.3.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
